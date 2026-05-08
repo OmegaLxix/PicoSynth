e8f91a3d417e1634b8996cc17837e55bf0dea072
OmegaLxix <161642454+OmegaLxix@users.noreply.github.com>
2026-05-07 23:14:26 -0400

    Update tusb_config.h

--- src/tusb_config.h
+++ src/tusb_config.h
@@ -1,6 +1,10 @@
 #ifndef TUSB_CONFIG_H_
 #define TUSB_CONFIG_H_
 
+#ifndef CFG_TUSB_OS
+#define CFG_TUSB_OS OPT_OS_NONE
+#endif
+
 #define CFG_TUSB_RHPORT0_MODE       (OPT_MODE_DEVICE)
 #define CFG_TUSB_OS                 OPT_OS_NONE
 #define CFG_TUSB_MCU                OPT_MCU_RP2040
@@ -9,9 +13,9 @@
 #define CFG_TUD_ENDPOINT0_SIZE      64
 
 // Audio Class
-#define CFG_TUD_AUDIO               1
-
-#define CFG_TUD_AUDIO_FUNC_COUNT    1
+#define CFG_TUD_AUDIO                      1
+#define CFG_TUD_AUDIO_FUNC_DESC_LEN_MAX    128
+#define CFG_TUD_AUDIO_FUNC_COUNT           1
 
 // Mono 16-bit speaker
 #define CFG_TUD_AUDIO_FUNC_1_N_AS_INT                1
